8dcc9e6b29e30411f85c7fae1f19f58ae61e3262
RTP-10305 <supawadee018@gmail.com>
2018-02-06 09:23:52 +0700

    Connected Firebase success

---
+++ app/src/main/java/piw/rmutsv/ac/th/firebaseservice/MainActivity.java
@@ -0,0 +1,30 @@
+package piw.rmutsv.ac.th.firebaseservice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import piw.rmutsv.ac.th.firebaseservice.fragment.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        if(savedInstanceState==null){
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment,new MainFragment())
+                    .commit();
+        }
+
+
+
+
+
+
+
+    }//Main Method
+}
