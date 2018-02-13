2fd8a43c44b88f8a2456901384ca748930904df7
RTP-10305 <supawadee018@gmail.com>
2018-02-13 11:49:21 +0700

    Signup

--- app/src/main/java/piw/rmutsv/ac/th/firebaseservice/fragment/MainFragment.java
+++ app/src/main/java/piw/rmutsv/ac/th/firebaseservice/fragment/MainFragment.java
@@ -1,12 +1,22 @@
 package piw.rmutsv.ac.th.firebaseservice.fragment;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import piw.rmutsv.ac.th.firebaseservice.R;
 
@@ -16,13 +26,68 @@ import piw.rmutsv.ac.th.firebaseservice.R;
 
 public class MainFragment extends Fragment {
 
+    private String emailString, passwordString;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+       //Register Controller
         registerCntroller();
+
+        //Login Controller
+        loginController();
     }
 
+    private void loginController() {
+        Button button = getView().findViewById(R.id.btnLogin);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText emailEditText = getView().findViewById(R.id.edtUser);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+                emailString = emailEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+
+                    checkEmailAndPass();
+
+
+            }
+        });
+    }
+
+    private void checkEmailAndPass() {
+
+
+        ProgressDialog progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setTitle("Please Wait Few Minus ...");
+        progressDialog.show();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signInWithEmailAndPassword(emailString,passwordString)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+//                            SignIn Success
+                            Toast.makeText(getActivity(), "Welcome To my Service",
+                                    Toast.LENGTH_SHORT).show();
+
+
+                        } else {
+//                            Cannot SignIn
+                            Toast.makeText(getActivity(), "Connot SignIn",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
 
 
     private void registerCntroller() {
--- app/src/main/java/piw/rmutsv/ac/th/firebaseservice/fragment/RegisterFragment.java
+++ app/src/main/java/piw/rmutsv/ac/th/firebaseservice/fragment/RegisterFragment.java
@@ -1,11 +1,19 @@
 package piw.rmutsv.ac.th.firebaseservice.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import piw.rmutsv.ac.th.firebaseservice.R;
 
@@ -14,8 +22,63 @@ import piw.rmutsv.ac.th.firebaseservice.R;
  */
 
 public class RegisterFragment extends Fragment {
+
+    //Intial value
+    private String nameString, emailString,passwordString;
+   private ImageView ImvSave;
+    private FirebaseAuth firebaseAuth;
     @Nullable
-    @Override
+
+
+   //setup Button
+
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        saveValueToFirebase();
+
+    }
+
+    private void saveValueToFirebase (){
+
+
+       ImvSave = getView().findViewById(R.id.imvSave);
+
+        ImvSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                firebaseAuth.createUserWithEmailAndPassword(emailString, passwordString)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()){
+                                    //Success
+                                    Toast.makeText(getActivity(),"Register Success",Toast.LENGTH_LONG).show();
+
+
+                                }
+                                else {
+                                    Toast.makeText(getActivity(),"!!!No register!!!",Toast.LENGTH_LONG).show();
+
+                                }
+                            }
+                        });
+
+
+            }
+        });
+
+
+
+
+    }//End Method saveValueToFirebase
+
+//end setup button
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_register,container,false);
         return view;
